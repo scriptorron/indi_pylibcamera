40c4bcbe7a0821c090e62adc19182a86b9f4239c
Aaron W Morris <aaron@aarmor.net>
2023-04-26 21:32:17 -0400

    install.sh performs full setup without user input

--- install.sh
+++ install.sh
@@ -1,11 +1,29 @@
-#!/usr/bin/sudo bash
+#!/usr/bin/bash
 
-if [ -z "$1" ]; then
-    echo "Usage: install.sh <path>/indy_pylibcamera.py"
-    exit 1
-fi
+set -o errexit
+set -o nounset
 
-cat > /usr/share/indi/indi_pylibcamera.xml <<EOL
+
+PATH=/usr/local/bin:/usr/bin:/bin
+export PATH
+
+
+# indiserver might be in /usr/local
+INDI_SERVER=$(which indiserver)
+INDI_SERVER_BIN=$(dirname "$INDI_SERVER")
+INDI_SERVER_USR=$(dirname "$INDI_SERVER_BIN")
+
+
+echo "Installing required packages for indi_pylibcamera"
+sudo apt-get update
+sudo apt-get install \
+    python3-picamera2 \
+    python3-lxml \
+    python3-astropy
+
+
+echo "Creating ${INDI_SERVER_USR}/share/indi/indi_pylibcamera.xml"
+sudo tee "${INDI_SERVER_USR}/share/indi/indi_pylibcamera.xml" >/dev/null <<EOL
 <driversList>
   <devGroup group="CCDs">
     <device label="INDI pylibcamera">
@@ -16,5 +34,17 @@ cat > /usr/share/indi/indi_pylibcamera.xml <<EOL
 </driversList>
 EOL
 
-ln -nsf $1 /usr/bin/indi_pylibcamera
-chmod +x /usr/bin/indi_pylibcamera
+
+GIT_DIR=$(dirname "$0")
+cd "$GIT_DIR"
+
+sudo ln -nsf "${PWD}/indi_pylibcamera.py" "${INDI_SERVER_USR}/bin/indi_pylibcamera"
+sudo chmod +x indi_pylibcamera.py
+
+cd "$OLDPWD"
+
+
+echo "You may now start the indiserver"
+echo ""
+echo "  indiserver indi_pylibcamera"
+
