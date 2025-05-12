7651a1e7f408ba79fcbb133fdd78d0b835f5070c
scriptorron <22291722+scriptorron@users.noreply.github.com>
2025-05-12 22:07:49 +0200

    WIP: implementing up to 5 driver instances

---
+++ src/indi_pylibcamera/indi_pylibcamera4.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from .indi_pylibcamera import main as main1
+
+
+def main(driver_instance="4"):
+    main1(driver_instance=driver_instance)
+
+
+if __name__ == "__main__":
+    main()
